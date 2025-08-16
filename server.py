257a10e4a410f167c967089a36801c752793c9af
sagiZ3 <sagizar444@gmail.com>
2025-08-16 04:59:07 +0300

    docs: hints and comments - were added. refactor: class functions and static. feat: added the option to logging, added access for client's info JSON file and arp cache for a batter performances and accessibility; added the option for resolving mac address using ip.

--- server.py
+++ server.py
@@ -1,5 +1,9 @@
 import socket
 import threading
+import os
+import subprocess
+import logging
+import json
 from time import sleep
 
 import protocol
@@ -8,10 +12,35 @@ from protocol import is_contain_profanity, WELCOME_MSG
 
 class Server:
     def __init__(self):
-        self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self._server_socket.bind(("0.0.0.0", protocol.PORT))
+        logging.basicConfig(level=logging.INFO)
+        self._server_socket: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        try:
+            self._server_socket.bind(("0.0.0.0", protocol.PORT))
+        except Exception as e:
+            logging.error("Failed to bind socket: " + str(e))
+            exit()
+
         self._server_socket.listen()
-        self._clients_dict = {}  # structure - client_socket: {"addr": addr, "nickname": nickname, "num_till_ban": num_till_ban)
+        self._clients_dict: dict = {}  # structure - client_socket: {"addr": addr, "nickname": nickname, "num_till_ban": num_till_ban)
+        logging.info("=====Server is up and running=====")
+
+        self._blocked_macs_data: None | str = None
+        self._arp_cache: dict[str: str] = {}  # structure: {IP: MAC}
+        self._ensure_blocked_macs_file()
+
+    def _ensure_blocked_macs_file(self) -> None:
+        """Creates blocked_macs.json file if it doesn't exist."""
+
+        filename: str = "blocked_macs.json"
+        if not os.path.exists(filename):
+            with open(filename, "w") as f:  # type: ignore
+                json.dump({"blocked_macs": []}, f, indent=4)  # type: ignore
+            logging.info("New JSON file was created for blocking evil users 📁")
+
+        with open(filename) as f:
+            self._blocked_macs_data = json.load(f)  # Inserts the JSON data into an accessible variable
+            logging.info("JSON file is now accessible!")
 
     def receive_client(self):
         while True:
@@ -19,25 +48,25 @@ class Server:
             try:
                 valid_nickname, nickname = protocol.get_msg(client_socket)
             except ConnectionResetError:
-                print("Client unexpectedly close the connection")
+                logging.warning("Client unexpectedly close the connection")
                 client_socket.close()
             else:
                 if valid_nickname:
                     self._clients_dict[client_socket] = {"addr": client_addr, "nick": nickname, "num_till_ban": 0}
-                    self.self_send(client_socket, WELCOME_MSG)
-                    self.broadcast(f"{nickname} joined the chat!")
-                    thread = threading.Thread(target=self.handle_client, args=(client_socket,))
+                    self._self_send(client_socket, WELCOME_MSG)
+                    self._broadcast(f"{nickname} joined the chat!")
+                    thread = threading.Thread(target=self._handle_client, args=(client_socket,))
                     thread.start()
                 else:
-                    self.self_send(client_socket, "Error with the nickname, please try again.")
+                    self._self_send(client_socket, "Error with the nickname, please try again.")
 
-    def handle_client(self, client_socket) -> None:
+    def _handle_client(self, client_socket) -> None:
         while True:
             try:
                 print(self._clients_dict)
                 valid_msg, client_msg = protocol.get_msg(client_socket)
             except ConnectionResetError:
-                print("Client unexpectedly close the connection")
+                logging.warning("Client unexpectedly close the connection")
                 self._clients_dict.pop(client_socket)
                 client_socket.close()
                 break
@@ -48,33 +77,59 @@ class Server:
                     break
                 else:
                     if is_contain_profanity(client_msg):
-                        self.broadcast(client_msg[:client_msg.find(':')+2] + len(client_msg[client_msg.find(':')+2:]) * '*')
+                        self._broadcast(client_msg[:client_msg.find(':') + 2] + len(client_msg[client_msg.find(':') + 2:]) * '*')
                         self._clients_dict[client_socket]['num_till_ban'] += 1
                         if self._clients_dict[client_socket]['num_till_ban'] == 3:
-                            self.self_send(client_socket, f"That's it!\n")
+                            self._self_send(client_socket, f"That's it!\n")
                             sleep(0.15)
-                            self.broadcast(self._clients_dict[client_socket]['nick'] + " has been permanently banned from using the safe chat, behave yourself!")
+                            self._broadcast(self._clients_dict[client_socket]['nick'] + " has been permanently banned from using the safe chat, behave yourself!")
                             sleep(0.15)
-                            self.self_send(client_socket, "BAN_SYN ")
+                            self._self_send(client_socket, "BAN_SYN ")
                             # TODO: add file that keeps the client IP for ban forever
                         else:
-                            self.self_send(client_socket, f"See Warning!\nYou have "
+                            self._self_send(client_socket, f"See Warning!\nYou have "
                                                           f"{3 - self._clients_dict[client_socket]['num_till_ban']}"
                                                           f" more violations left before being banned")
                     else:
-                        self.broadcast(client_msg)
+                        self._broadcast(client_msg)
             else:
-                self.self_send(client_socket, "Error in message.")
+                self._self_send(client_socket, "Error in message.")
                 client_socket.recv(1024)  # Attempt to empty the socket from possible garbage
 
+    def _is_user_blocked(self, client_ip: str) -> bool:
+        for item in self._blocked_macs_data["blocked_macs"]:
+            if item["mac"] == self._extracts_user_mac_from_ip(client_ip):
+                if item["past_warnings"] == 3:
+                    return True
+            else:
+                return False
+        return False
 
-    @staticmethod
-    def self_send(client_socket, msg):
-        client_socket.send(protocol.create_msg(msg))
+    def _add_1_client_profanity_warning(self, client_ip: str) -> None:
+        for item in self._blocked_macs_data["blocked_macs"]:
+            if item["mac"] == self._extracts_user_mac_from_ip(client_ip):
+                item["past_warnings"] += 1
 
-    def broadcast(self, msg):
+    def _extracts_user_mac_from_ip(self, ip: str) -> str:
+        if ip in self._arp_cache:
+            return self._arp_cache[ip]
+
+        try:
+            client_mac: str = subprocess.check_output(f"arp -a {ip}", shell=True)\
+                .decode().split(ip)[1].lstrip().split(" ")[0]
+            return client_mac
+        except Exception as e:
+            logging.info("Problem with finding the client's MAC address: " + str(e))
+        finally:
+            return ""  # consider in the check
+
+    def _broadcast(self, msg):
         for client_socket in self._clients_dict.keys():
             client_socket.send(protocol.create_msg(msg))
+
+    @staticmethod
+    def _self_send(client_socket, msg):
+        client_socket.send(protocol.create_msg(msg))
 
 
 if __name__ == '__main__':
