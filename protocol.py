cd407923bf97dfa678f3863a9dbcb683430170d1
sagiZ3 <sagizar444@gmail.com>
2025-08-17 01:01:04 +0300

    structure: later, the lines where the comments are specified will be deleted, passed to specific constants style: added variables&functions hints

--- protocol.py
+++ protocol.py
@@ -1,11 +1,10 @@
-import google.generativeai as gen_ai
-
-from socket import socket
-from colorama import Fore
+import google.generativeai as gen_ai  # delete - in different constants
 import socket
 
+from colorama import Fore  # delete?
 
-# ================ Gemini Section ================
+
+# ================ Gemini Section ================  # delete - in different constants
 
 GEMINI_API_KEY = "X"
 
@@ -28,31 +27,31 @@ def is_contain_profanity(payload):
 
 
 LENGTH_FIELD_SIZE = 2
-CONNECTION_PORT = 8888
-LISTEN_EVERYONE_IP = "0.0.0.0"
-LISTEN_LOOPBACK_IP = "127.0.0.1"
-SERVER_IP = socket.gethostbyname(socket.gethostname())  # change if run on different computers
+CONNECTION_PORT = 8888  # delete - in different constants
+LISTEN_EVERYONE_IP = "0.0.0.0"  # delete - in different constants
+LISTEN_LOOPBACK_IP = "127.0.0.1"  # delete - in different constants
+SERVER_IP = socket.gethostbyname(socket.gethostname())  # delete - in different constants
 
-WELCOME_MSG = "~~Welcome To Safe Chat~~"
-USERNAME_CONTAIN_PROFANITY_RESPONSE = "Please enter a name that doesn't include any kind of profanity!"
-EMPTY_USERNAME_RESPONSE = "Please enter a name!"
-COLOR = Fore.LIGHTBLUE_EX
-EXIT_COLOR = Fore.RED
+WELCOME_MSG = "~~Welcome To Safe Chat~~"  # delete - in different constants
+USERNAME_CONTAIN_PROFANITY_RESPONSE = "Please enter a name that doesn't include any kind of profanity!"  # delete - in different constants
+EMPTY_USERNAME_RESPONSE = "Please enter a name!"  # delete - in different constants
+COLOR = Fore.LIGHTBLUE_EX  # delete?
+EXIT_COLOR = Fore.RED  # delete?
 
 
-def create_msg(data):
+def create_msg(data) -> bytes:
     """Create a valid protocol message, with length field."""
     return f"{str(len(data)).zfill(LENGTH_FIELD_SIZE)}{data}".encode()
 
 
-def get_msg(my_socket: socket):
+def get_msg(my_socket: socket.socket) -> tuple[bool, str]:
     """Extract message from protocol, without the length field.
        If length field does not include a number, returns False, "Error" """
     message = my_socket.recv(101).decode()  # TODO: add conversation option for getting data length
     payload_len = message[:LENGTH_FIELD_SIZE]
     payload = message[LENGTH_FIELD_SIZE:]
 
-    if payload_len.isnumeric() and int(payload_len) == len(payload):
+    if payload_len.isdigit() and int(payload_len) == len(payload):
         return True, payload
 
     return False, "Error"
