b5de431fddf186549d85e1e2a15b44eddd40b125
sagiZ3 <sagizar444@gmail.com>
2025-08-16 04:48:26 +0300

    style,docs: hints, spaces and comments - were added, structure.

--- client.py
+++ client.py
@@ -6,13 +6,13 @@ from protocol import is_contain_profanity
 
 class Client:
     def __init__(self):
-        self._my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._my_socket: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._my_socket.connect((protocol.SERVER_IP, protocol.PORT))
-        self._nickname = ""
+        self._nickname: str = ""
         self.messages_lst: list = []
         self.server_special_messages_dict = {"EXIT ": lambda: f"{self._nickname} left the chat!",
-                                             "BAN_ACK ": lambda: "BAN_ACK "
-                                            } # a space so that the client cannot imitate the message
+                                             "BAN_ACK ": lambda: "BAN_ACK "  # lambda uses for customise the nickname every time
+                                            }  # a space so that the client cannot imitate the message
 
     def is_username_includes_profanity(self, nickname) -> bool:
         if not is_contain_profanity(nickname):
@@ -21,7 +21,7 @@ class Client:
             return False
         return True
 
-    def send_msg(self, payload):  # see if X window button include in the except statement - if at server it doesn't work
+    def send_msg(self, payload) -> None:
         client_msg = f"{self._nickname}: {payload}"
         if payload in self.server_special_messages_dict:
             client_msg = self.server_special_messages_dict[payload]()
@@ -29,10 +29,10 @@ class Client:
         client_msg = protocol.create_msg(client_msg)
         self._my_socket.send(client_msg)
 
-    def receive_msg(self):  # try needed?
+    def receive_msg(self) -> None:
         try:
             while True:
-                valid_msg, msg = protocol.get_msg(self._my_socket)  # issue needed - ACK for EXIT
+                valid_msg, msg = protocol.get_msg(self._my_socket)
                 if valid_msg:
                     self.messages_lst.append(msg)
                 else:
